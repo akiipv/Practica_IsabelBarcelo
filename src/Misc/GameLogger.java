b0f6f381ac0cbecbada6645dfee37c4f2cfe70fc
María Isabel Barceló Velasco <17251@iesabyla.es>
2026-02-18 17:21:41 +0100

    tengo q rescribir el compareTo y eso

--- src/Misc/GameLogger.java
+++ src/Misc/GameLogger.java
@@ -8,12 +8,27 @@ import java.io.IOException;
 
 public class GameLogger {
 
-    public static void Fichita(Personaje player) throws IOException {
+    public static void cardIB(Personaje player) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter("./" + player.getNombre() + ".txt"));
 
         bw.newLine();
         bw.write(player.toString2());
         bw.close();
+    }
+
+    public static void cardIB(Personaje[] player) throws IOException {
+
+        for (int i = 0; i < player.length; i++) {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("./" + player[i].getNombre() + ".txt"));
+
+            bw.newLine();
+            bw.write(player[i].toString2());
+            bw.close();
+        }
+    }
+
+    public static void sortArrayito(Personaje[] player) throws IOException {
+
     }
 
 }
