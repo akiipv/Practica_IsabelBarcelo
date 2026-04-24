364024bbf9733b0455ed3ca65ac719a04e6e783d
akiipv <17251@iesabyla.es>
2026-04-24 12:24:15 +0200

    he dormido fatal no tengo ni ganas d poner kaomojis
    tengo depresion y probablemente después del examen d hoy voy a hacer un backflip
    
    he dejado el 7 sin terminar. tengo q corregir el constructor por parámetros y nose q más m falta

--- src/GameMap/Mazmorra.java
+++ src/GameMap/Mazmorra.java
@@ -1,4 +1,84 @@
 package GameMap;
 
+import Characters.Monstruo;
+import Characters.Personaje;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Mazmorra {
+    private String nombre;
+    private int nivel;
+    private ArrayList<Monstruo> monstruos;
+
+    public Mazmorra() {
+        setNombre("");
+        setNivel(0);
+        monstruos = new ArrayList<>();
+    }
+
+    public Mazmorra(File fichero) {
+        /*setNombre(nombre);
+        setNivel(nivel);
+        setMonstruos(monstruos);
+        No había leído lo del fichero, lo tengo que cambiar ;C*/
+    }
+
+    public Mazmorra(Mazmorra m) {
+        setNombre(m.getNombre());
+        setNivel(m.getNivel());
+        setMonstruos(m.getMonstruos());
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public ArrayList<Monstruo> getMonstruos() {
+        return monstruos;
+    }
+
+    public void setMonstruos(ArrayList<Monstruo> monstruos) {
+        ArrayList<Monstruo> depresion = new ArrayList<>();
+        for (Monstruo m : monstruos) {
+            if (!depresion.contains(m) &&
+                    m.getNivel() <= this.getNivel() + 3 &&
+                    m.getNivel() >= this.getNivel() - 3 ) depresion.add(m);
+        }
+        this.monstruos = new ArrayList<>(depresion);
+    }
+
+    public Monstruo combateRandom(){
+        if (getMonstruos().isEmpty()) return null;
+
+        Random r  = new Random();
+        int i = r.nextInt(getMonstruos().size());
+
+        return getMonstruos().get(i);
+    }
+
+    @Override
+    public String toString() {
+        String res = "Cargando datos de la mazmorra.." +
+                "\n· Nombre: " + getNombre() +
+                "\n· Nivel: " + getNivel() +
+                "\n Monstruos: ";
+        for(Monstruo m : getMonstruos()) {
+            res += "\n\t" + m.toString() + "\n";
+        }
+        return res;
+    }
 }
--- src/Misc/Main.java
+++ src/Misc/Main.java
@@ -1,14 +1,23 @@
 package Misc;
 
 import Characters.*;
-import Gear.Artefacto;
+import GameMap.*;
+import Gear.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Personaje p = new Paladin("Iván", 99, 99, 99, 99, 99, 99, 99);
-        GameLogger.cardIB(p);
+        ArrayList<Monstruo> monstruos = new ArrayList<>();
+        monstruos.add(new Monstruo("pepe", 5, 7, 3, 6, 2, 4, "bestia"));
+        monstruos.add(new Monstruo("juan", 3, 4, 8, 2, 5, 6, "no-muerto"));
+        monstruos.add(new Monstruo("ivan", 9, 10, 7, 8, 6, 5, "gigante"));
+        monstruos.add(new Monstruo("cristian", 6, 5, 4, 7, 3, 2, "bestia"));
+        monstruos.add(new Monstruo("carmen", 4, 6, 9, 3, 7, 8, "no-muerto"));
+        Mazmorra m = new Mazmorra("ola", 1, monstruos);
+
+        System.out.println(m.getMonstruos().toString());
     }
 }
